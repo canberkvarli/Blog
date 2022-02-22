052f9e78650c044e8d9a79deb8823bd85cdf3b6f
canberko560 <canberkvarli@gmail.com>
2022-02-22 22:33:38 +0300

    add articles controller, model, and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_145204) do
+ActiveRecord::Schema.define(version: 2022_02_22_192440) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
