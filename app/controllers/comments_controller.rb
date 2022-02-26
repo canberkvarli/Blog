d69b6fe71f9bc94dfb0c355c8131ac7ec321d8ab
canberko560 <canberkvarli@gmail.com>
2022-02-26 17:21:45 +0300

    add destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,14 @@ class CommentsController < ApplicationController
 
     end
 
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+
+        redirect_to article_path, status: 303
+    end
+
     private
     def comment_params
         params.require(:comment).permit(:commenter, :body, :status)
