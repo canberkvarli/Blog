c2f8d27c20cac3c116582b0a60a654f0c5e5c25a
canberko560 <canberkvarli@gmail.com>
2022-02-26 17:13:40 +0300

    add module

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,12 +9,15 @@
 #  updated_at :datetime         not null
 #
 class Article < ApplicationRecord
-
+    include Visible
+    
     validates :title, presence: :true
     validates :body, presence:  :true, length: {minimum: 10}
 
+    
     has_many :comments, 
     class_name: "Comment",
     dependent: :destroy
+
 
 end
